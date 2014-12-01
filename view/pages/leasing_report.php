fb90c1865bd45375c6b9dece395a55a5628430b5
senlin13gt <senlin13gt@gmail.com>
2014-12-01 14:45:39 -0500

    add missing files

---
+++ view/pages/leasing_report.php
@@ -0,0 +1,40 @@
+<p> 3 Month leasing report </p>
+<?php
+
+$query = "SELECT MONTHNAME(PR.Pref_Move) AS Lease_Month, A.Category, COUNT(A.Apt_No) AS nums_of_Apartments
+          FROM Apartment AS A, Prospective_Resident AS PR, Resident AS R
+          WHERE A.Apt_No = R.Apt_No AND PR.Username = R.Username
+          AND MONTH(PR.Pref_Move) in (8,9,10)
+          AND MONTH(PR.Pref_Move) <= MONTH(now())
+          AND YEAR(PR.Pref_Move) = YEAR(now())
+          GROUP BY MONTHNAME(PR.Pref_Move), A.Category 
+          ORDER BY PR.Pref_Move ASC;";
+ ?>
+ <? if ($result = db()->query($query)) { ?>
+    <table>
+      <thead>
+        <tr>
+           <td>Month</td>
+           <td>Category</td>
+           <td>No. Of Apartments</td>
+         </tr>
+      </thead>
+      <tbody>
+        <? $month = ''; ?>
+        <? while ($row = $result->fetch_assoc()) { 
+        ?>
+           <tr>
+            <td><?php echo ($month == $row['Lease_Month']) ? '' : $row['Lease_Month']; ?></td>
+            <td><?php echo $row['Category']; ?></td>
+            <td align ="center"><?php echo $row['nums_of_Apartments']; ?></td>
+          </tr>
+          <?php $month = $row['Lease_Month'];?>
+        <?
+          }
+        ?>
+      <tbody>
+    </table>
+<?
+}
+?>
+
---
+++ view/pages/service_report.php
@@ -0,0 +1,40 @@
+<p> Service Request Resolution Report </p>
+<?php
+$query = "SELECT  MONTHNAME(Date_Of_Request) AS Month, Issue_Type, AVG(DATEDIFF(Date_Resolved, Date_Of_Request)) AS Avg_Days_To_Resolve
+			FROM Maintenance_Request 
+			WHERE MONTH(Date_Of_Request) <= MONTH(now()) 
+			AND MONTH(Date_Of_Request) in (8,9,10)
+			AND Issue_Status = 'resolved'
+			AND YEAR(Date_Of_Request) = YEAR(now()) 
+			GROUP BY MONTHNAME(Date_Of_Request), Issue_Type
+			ORDER BY MONTH(Date_Of_Request) ASC;";
+ ?>
+ <? if ($result = db()->query($query)) { ?>
+    <table>
+      <thead>
+        <tr>
+           <td>Month</td>
+           <td>Type of Request</td>
+           <td>Average No of Days</td>
+         </tr>
+      </thead>
+      <tbody>
+        <? $month = ''; ?>
+        <? while ($row = $result->fetch_assoc()) { 
+        ?>
+           <tr>
+            <td><?php echo ($month == $row['Month']) ? '' : $row['Month']; ?></td>
+            <td><?php echo $row['Issue_Type']; ?></td>
+            <td><?php echo $row['Avg_Days_To_Resolve']; ?></td>
+          </tr>
+          <?php $month = $row['Month'];?>
+        <?
+          }
+        ?>
+      <tbody>
+    </table>
+<?
+}
+?>
+
+  
